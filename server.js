0487fb59c3f0f9affdbb38a74aace5cd3f2adf87
Bilo Lwabona <bilo.lwabona@gmail.com>
2017-06-07 01:13:19 +0200

    working on server-side routing

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 var express = require('express');
+var path = require('path');
 var server = express();
-var port = process.env.port || 2700;
+var port = process.env.port || 2010;
+
+// https://stackoverflow.com/questions/28553904/client-routing-using-react-router-and-server-side-routing
+server.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/dist/'));
+});
 
 server.use(express.static(__dirname + '/dist/'));
 server.listen(port, '0.0.0.0');
 console.log(`
- ______  _____         _____    _____  _____ 
- |_____]   |   |      |     |     |   |     |
- |_____] __|__ |_____ |_____| . __|__ |_____|
- 
+GO2 APP Running! 
  `)
 console.log('listening on port: ' + port);
