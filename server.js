e692550a6a0f6a660af16a734ffb294e5d3abd80
Unknown <bilo.lwabona@gmail.com>
2017-06-08 17:25:00 +0200

    added express-history-api-fallback

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var fallback = require('express-history-api-fallback');
 var server = express();
 var port = process.env.port || 2010;
 
@@ -8,6 +9,7 @@ server.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname + '/dist/'));
 });
 
+server.use(fallback('index.html', {root:__dirname + '/dist/'}));
 server.use(express.static(__dirname + '/dist/'));
 server.listen(port, '0.0.0.0');
 console.log(`
