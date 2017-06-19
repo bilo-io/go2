0d3a3df28c2ad57ac0d6062fb9a78467a8640f54
Unknown <bilo.lwabona@gmail.com>
2017-06-19 20:08:03 +0200

    migrated to different project setup

--- server.js
+++ server.js
@@ -1,15 +1,72 @@
-var express = require('express');
-var path = require('path');
-var fallback = require('express-history-api-fallback');
-var server = express();
-var port = process.env.port || 2001;
+var express = require('express'),
+      httpProxy = require('http-proxy'),
+      request = require('request'),
+      url = require("url"),
+      server = express(),
+      port = process.env.port || 2700,
+      proxy = httpProxy.createProxyServer({ secure: false, forward: "https://platform.whereismytransport.com" }),
+      token,
+      tapi = {
+            platformApiUrl: 'https://platform.whereismytransport.com/api',
+            identityStsUrl: 'https://identity.whereismytransport.com/connect/token',
+            clientId: 'e178dbdd-57e3-4735-8825-b432a9377618',
+            clientSecret: 'hbDfw04ds+yfdPWiERFg+E1kH9ifmu/+92/lHNMJ4Co='
+      }
 
-server.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/dist/'));
+function getToken(callback) {
+      request.post("https://identity.whereismytransport.com/connect/token",
+            {
+                  form: {
+                        client_id: tapi.clientId,
+                        client_secret: tapi.clientSecret,
+                        grant_type: "client_credentials",
+                        scope: "transportapi:all"
+                  }
+            },
+            function (error, response, body) {
+                  if (error == null && response.statusCode == 200) {
+                        token = "Bearer " + JSON.parse(body).access_token;
+                        callback();
+                  }
+            }
+      );
+};
+
+function proxyRequest(e) {
+      var parsedUrl = url.parse(e.req.url, true);
+      e.req.headers["authorization"] = token;
+      proxy.web(e.req, e.res, { target: "https://platform.whereismytransport.com", changeOrigin: true }, function (error) {
+            console.log("error" + error);
+      });
+}
+/**
+ * gets the token every hour
+ */
+setInterval(function () {
+      getToken();
+}, 1000 * 3600);
+/**
+ * NB: checks all requests that have /api in them
+ * it tries to proxy ALL requests that have /api in them.
+ * So if you have anything with /api and it does not 
+ * interact with transportapi.. it might not work.
+ * 
+ * All requests that it does not filter on get forwarded on in the pipeline
+ */
+server.use(function (req, res, next) {
+      console.log("request: ", req.url);
+      if (req.url.indexOf("/api") !== -1) {
+            console.log("proxying request");
+            proxyRequest({ res, req })
+      } else {
+            next();
+      }
 });
 
-server.use(fallback('index.html', {root:__dirname + '/dist/'}));
 server.use(express.static(__dirname + '/dist/'));
-server.listen(port, '0.0.0.0');
 
-console.log(`go-2:server.js listening on port:${port}`);
+getToken(() => {
+      server.listen(port, '0.0.0.0', server => {
+            console.log('listening on port: ' + port);
+      });
+})
--- webpack.config.js
+++ webpack.config.js
@@ -6,47 +6,35 @@ var SRC = path.resolve(__dirname, 'src/');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var config = {
-    devtool: 'source-map',
     entry: SRC + '/index.js',
     output: {
         path: DIST,
         publicPath: '/',
-        filename: 'app.js'
-
+        filename: 'app.js',
+        sourceMapFilename: '[file].map',
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }, {
-                test: /\.jsx$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }, {
-                test: /\.scss$/,
-                loaders: [
-                    'style-loader', 'css-loader', 'sass-loader'
-                ],
-                exclude: /node_modules/
-            }, {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'file-loader?name=assets/[name].[ext]'
-            }, {
-                test: /\.md$/,
-                use: [
-                    {
-                        loader: 'html-loader'
-                    }
-                ]
-            }
-        ]
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.jsx$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.scss$/,
+            loaders: ['style-loader', 'css-loader', 'sass-loader'],
+            exclude: /node_modules/
+        }, {
+            test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+            loader: 'file-loader?name=assets/[name].[ext]',
+        }]
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html', 
-            filename: 'index.html', 
+            template: './src/index.html',
+            filename: 'index.html',
             inject: 'body'
         })
     ],
@@ -54,7 +42,7 @@ var config = {
         historyApiFallback: true,
         stats: 'minimal'
     },
-    // node: {     fs: 'empty' }
+    devtool: 'source-map'
 };
 
 module.exports = config;
